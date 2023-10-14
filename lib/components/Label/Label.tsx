f3cced63859779c3c6d8b116fed9c19134d48fd4
antonioflore <floreantonio@hotmail.it>
2023-10-14 19:54:50 +0200

    added Label component and extend Button with it

--- lib/components/Label/Label.tsx
+++ lib/components/Label/Label.tsx
@@ -1,30 +1,30 @@
 import './Label.css';
-import { FontFamilies, LabelTypes, TextSizes } from './labelTypes';
+import { FontFamilies, TextSizes } from './labelTypes';
 
 /**
  * Props for a label component.
  * @interface
  */
 export interface LabelProps {
-    /**
-     * The label text to display.
-     */
-    label: string;
+  /**
+   * The label text to display.
+   */
+  label: string;
 
-    /**
-     * Additional CSS class for styling the label component.
-     */
-    labelType?: LabelTypes;
+  /**
+   * Additional CSS class for styling the label component.
+   */
+  labelType?: 'in-stock' | 'selected';
 
-    /**
-     * The font family to use for the label text.
-     */
-    fontFamily: FontFamilies;
+  /**
+   * The font family to use for the label text.
+   */
+  fontFamily: FontFamilies;
 
-    /**
-     * The size of the label.
-     */
-    textSize?: TextSizes;
+  /**
+   * The size of the label.
+   */
+  textSize?: TextSizes;
 }
 
 /**
@@ -33,6 +33,16 @@ export interface LabelProps {
  * @param {LabelProps} props - The props for the Label component.
  * @returns {JSX.Element} - The JSX element representing the label.
  */
-export const Label = ({ label, labelType, fontFamily, textSize = TextSizes.Sm, ...props }: LabelProps):JSX.Element => {
-  return <label className={`${fontFamily} label-${textSize} label-${labelType}`} {...props}>{label}</label>;
+export const Label = ({
+  label,
+  labelType,
+  fontFamily,
+  textSize = TextSizes.Sm,
+  ...props
+}: LabelProps): JSX.Element => {
+  return (
+    <label className={`${fontFamily} label-${textSize} label-${labelType}`} {...props}>
+      {label}
+    </label>
+  );
 };
