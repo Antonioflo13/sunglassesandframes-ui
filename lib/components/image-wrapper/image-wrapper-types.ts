4dab970dd7641ad94650c3379d1099eb1323e875
antonioflore <floreantonio@hotmail.it>
2023-10-23 10:37:42 +0200

    added Img component for reder in ImageWrapper stories

--- lib/components/image-wrapper/image-wrapper-types.ts
+++ lib/components/image-wrapper/image-wrapper-types.ts
@@ -1,5 +1,11 @@
 export type ImageWrapperProps = {
+  /**
+   * Additional React elements or content to display inside the image wrapper
+   */
   children: React.ReactNode;
+  /**
+   * The label text to display
+   */
   typeImage: TypeImage;
 };
 
