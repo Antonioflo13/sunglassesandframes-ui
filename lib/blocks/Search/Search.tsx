927dc28eca4f20ae8cd531c9c87a009365ff3c46
GiovanniP <pignataro.giovanni@hotmail.com>
2023-12-19 21:52:26 +0100

    fix left css

--- lib/blocks/Search/Search.tsx
+++ lib/blocks/Search/Search.tsx
@@ -13,14 +13,14 @@ export const Search = ({ designers, products }: SearchProps): JSX.Element => {
     <section className={styles['container']}>
       <SearchInput />
       <Typography className={styles['title-desktop']} label={`DESIGNER (${designers.length})`} fontFamily="helvetica-regular" textSize="lg" />
-      <div className={`${styles['container-results']} ${styles[`${designers.length === 1 ? 'single-item' : 'more-items'}`]}`}>
+      <div className={`${styles['results-designers']} ${styles[`${designers.length === 1 ? 'single-item' : 'more-items'}`]}`}>
         {designers.map(designer => (
           <Typography key={designer.id} label={designer.name} fontFamily="helvetica-regular" textSize="lg" />
         ))}
       </div>
       <Typography className={styles['title-desktop']} label={`PRODUCTS (${products.length})`} fontFamily="helvetica-regular" textSize="lg" />
-      <div className={styles['container-results']}>
-        <Grid type="products" items={products} />
+      <div className={styles['results-products']}>
+        <Grid className='grid' type="products" items={products} />
       </div>
     </section>
   );
