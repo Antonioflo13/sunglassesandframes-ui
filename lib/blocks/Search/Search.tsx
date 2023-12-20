967c4c4818ded80df055a0fa1ba96468c8b0dfd4
GiovanniP <pignataro.giovanni@hotmail.com>
2023-12-20 11:02:27 +0100

    fix css

--- lib/blocks/Search/Search.tsx
+++ lib/blocks/Search/Search.tsx
@@ -2,25 +2,59 @@
 import { Grid } from '@blocks/Grid';
 import { SearchInput } from '@components/SearchInput';
 import { Typography } from '@components/Typography';
+import { useState } from 'react';
 
 import styles from './Search.module.css';
 
 import { SearchProps } from '.';
 
 export const Search = ({ designers, products }: SearchProps): JSX.Element => {
+  const [selectionsLabel, setSelectionsLabel] = useState([
+    {
+      label: 'DESIGNERS',
+      selected: true,
+    },
+    {
+      label: 'PRODUCTS',
+      selected: false,
+    },
+  ]);
+
+  const onClickLabel = (clickedLabel: string): void => {
+    const updatedSelections = selectionsLabel.map(selection => {
+      return {
+        ...selection,
+        selected: selection.label === clickedLabel,
+      };
+    });
+
+    return setSelectionsLabel(updatedSelections);
+  };
 
   return (
     <section className={styles['container']}>
+      <div className={styles['mobile-filter-search']}>
+        {selectionsLabel.map((selection, key) => (
+          <button key={key} onClick={(): void => onClickLabel(selection.label)}>
+            <Typography
+              className={styles[`${selection.selected && 'label-selected'}`]}
+              label={selection.label}
+              fontFamily="helvetica-light"
+              textSize="lg"
+            />
+          </button>
+        ))}
+      </div>
       <SearchInput />
       <Typography className={styles['title-desktop']} label={`DESIGNER (${designers.length})`} fontFamily="helvetica-regular" textSize="lg" />
-      <div className={`${styles['results-designers']} ${styles[`${designers.length === 1 ? 'single-item' : 'more-items'}`]}`}>
-        {designers.map(designer => (
-          <Typography key={designer.id} label={designer.name} fontFamily="helvetica-regular" textSize="lg" />
+      <div className={styles['search-designers']}>
+        {designers.map((designer, key) => (
+          <Typography key={key} label={designer.name} fontFamily="helvetica-regular" textSize="lg" />
         ))}
       </div>
       <Typography className={styles['title-desktop']} label={`PRODUCTS (${products.length})`} fontFamily="helvetica-regular" textSize="lg" />
-      <div className={styles['results-products']}>
-        <Grid className='grid' type="products" items={products} />
+      <div className={styles['search-products']}>
+        <Grid type="products" items={products} />
       </div>
     </section>
   );
