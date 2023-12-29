7e37fbe110e5c972d384a377cff8886479dd69a1
GiovanniP <pignataro.giovanni@hotmail.com>
2023-12-29 15:29:33 +0100

    css for SearchResultsNotFound

--- lib/components/SearchResultsNotFound/SearchResultsNotFound.tsx
+++ lib/components/SearchResultsNotFound/SearchResultsNotFound.tsx
@@ -1,12 +1,15 @@
 
+import { IconWrapper } from '@components/IconWrapper';
 import { Typography } from '@components/Typography';
+
+import styles from './SearchResultsNotFound.module.css';
 
 import { SearchResultsNotFoundProps } from '.';
 
 export const SearchResultsNotFound = ({ icon, labelNotFound }: SearchResultsNotFoundProps): JSX.Element => {
   return (
-    <section>
-      <img src={icon} />
+    <section className={styles['container']}>
+      <IconWrapper size={150} src={icon} alt='not found' />
       <Typography label={labelNotFound} fontFamily="helvetica-regular" />
     </section>
   );
